9f1f2cb7886de252ce4a4126b7fd4ca6a8aa32c6
PederHatlen <pehaa002@osloskolen.no>
2022-03-21 14:37:41 +0100

    Oppgave5

--- Oppgave5/js/script.js
+++ Oppgave5/js/script.js
@@ -1,20 +1,37 @@
 let outputBoxEl = document.getElementById("outputBox");
 let formEl = document.getElementById("form");
-// let fooditems = document.getElementById("fooditems");
+let fooditemsEl = document.getElementById("fooditems");
+let newFoodEl = document.getElementById("newFood");
 
-let elements = document.getElementsByTagName("input");
+let elements = formEl.getElementsByTagName("input");
 for (let i = 0; i < elements.length; i++) {
     console.log(elements[i]);
-    elements[i].addEventListener("input", formSubmit);
+    elements[i].addEventListener("input", makeString);
 }
 
-function formSubmit(e){
-    e.preventDefault;
-    console.log(e);
-    let data = [...new FormData(formEl)];
-    outputBoxEl = "<p> "+data;
+function makeString(e){
+    // e.preventDefault;
+    let fData = new FormData(formEl);
+    let data = [...fData];
+    console.log(e,data);
+    
+    let name = (fData.get("name") != ""? "Jeg heter "+fData.get("name")+". ":"");
+    let age = (fData.get("age") != ""? "Jeg er "+fData.get("age")+" år gammel. ":"");
+    let drink = (fData.get("drink") != null? "Min favoritt drikke er "+fData.get("drink")+". ":"");
+    let foodarray = fData.getAll("food");
+    let food = "";
+
+    if(foodarray.length > 1){
+        food = "Noen matretter jeg liker er "+foodarray.slice(0, -1).join(", ")+" og "+foodarray.slice(-1)+". ";
+        console.log(food);
+    }else if(foodarray.length == 1){
+        food = "Jeg liker "+foodarray+". "
+    }
+
+    outputBoxEl.innerHTML = "<p>"+name+age+drink+food+"</p>";
 }
 
 function newFood(e){
-
+    console.log(e);
+    fooditemsEl.innerHTML += "<label for=\""+newFoodEl.value+"\">"+newFoodEl.value+"</label><input type=\"checkbox\" name=\"food\" id=\"food"+newFoodEl.value+"\" value=\""+newFoodEl.value+"\" onClick=\"makeString(event)\">";
 }
